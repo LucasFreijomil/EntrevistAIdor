c39836c0fa0a08f1af23bff30b1971163b635cf0
Lucas Freijomil <lucasfreijomil@gmail.com>
2024-10-25 12:08:01 -0300

    basic-welcome

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useLocation } from "react-router-dom";
 import Landing from "./Components/Landing/Landing";
 import Chatbot from "./Components/Chatbot/Chatbot";
+import "./index.css"
 
 const App = () => {
   const {pathname} = useLocation();
